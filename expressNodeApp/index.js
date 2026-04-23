267d2f60a139f4c15c97d9906f4ccbe6745d6e7e
Arzel Serdeña <arzeldserdena@iskolarngbayan.pup.edu.ph>
2026-04-23 20:31:58 +0800

    Fixed working directory path for azure

--- expressNodeApp/index.js
+++ expressNodeApp/index.js
@@ -40,5 +40,5 @@ app.use("/submit", submitSurveyForm);
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log("Server running on port ${PORT");
+  console.log(`Server running on port ${PORT}`);
 });
