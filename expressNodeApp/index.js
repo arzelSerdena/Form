55a8649cc9cea2a36c94c9d78d776126b50a1b9f
Arzel Serdeña <arzeldserdena@iskolarngbayan.pup.edu.ph>
2026-04-24 00:03:15 +0800

    Added logo for server status page

--- expressNodeApp/index.js
+++ expressNodeApp/index.js
@@ -38,5 +38,5 @@ mongoose
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port http://localhost:${PORT}`);
 });
